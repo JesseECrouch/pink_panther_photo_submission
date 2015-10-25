0df52641d5fdb212857d663c340f3b20acd2c542
Prelang Builder <builder@prelang.com>
2015-10-25 18:03:59 +0000

    Made model 'Post' votable by adding acts_as_votable

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ActiveRecord::Base
   validates_formatting_of :email_address, using: :email
   validates_formatting_of :zip_code, using: :us_zip
   validates_formatting_of :link_to_photo, using: :url
+  acts_as_votable 
 end
