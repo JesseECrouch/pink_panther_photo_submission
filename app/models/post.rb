8fa7d9d1d8ac9a55490d78c6e832e98a9c28e0b5
Prelang Builder <builder@prelang.com>
2015-10-25 18:03:57 +0000

    Defined validation for attribute 'link_to_photo' on model 'Post'

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ActiveRecord::Base
   belongs_to :user
   validates_formatting_of :email_address, using: :email
   validates_formatting_of :zip_code, using: :us_zip
+  validates_formatting_of :link_to_photo, using: :url
 end
